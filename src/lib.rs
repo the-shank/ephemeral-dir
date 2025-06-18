6726a2ce4859a56d85cf6902c22a1dfee786c273
Shank <shashanksharma21@gmail.com>
2025-06-18 07:28:01 -0400

    create ephemeral dir

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,34 @@
+use std::fs;
 use std::path::{Path, PathBuf};
 
-struct EphemeralDir {
+pub struct EphemeralDir {
     // TODO: should we use a Box<Path> here instead of PathBuf?
     path: PathBuf,
 }
 
 impl EphemeralDir {
-    pub fn new(path: impl AsRef<Path>) -> Self {
-        todo!()
+    pub fn new(path: impl AsRef<Path>) -> eyre::Result<Self> {
+        let path = path.as_ref();
+        assert!(!path.exists());
+
+        fs::create_dir_all(path)?;
+
+        Ok(Self {
+            path: PathBuf::from(path),
+        })
+    }
+
+    pub fn new_forced(path: impl AsRef<Path>) -> eyre::Result<Self> {
+        let path = path.as_ref();
+        if path.exists() {
+            fs::remove_dir_all(path)?;
+        }
+
+        Self::new(path)
+    }
+
+    pub fn path(&self) -> &Path {
+        self.path.as_ref()
     }
 }
 
@@ -19,6 +40,13 @@ impl Drop for EphemeralDir {
 
 #[test]
 fn test_dir_exists_after_creation() {
+    let path = "/tmp/ephemeral_dir_test";
+    let res_dir = EphemeralDir::new_forced(path);
+    assert!(res_dir.is_ok());
+    let Ok(dir) = res_dir else { unreachable!() };
+    assert!(dir.path().exists());
+    let dir_path_str = format!("{}", dir.path().display());
+    assert_eq!(dir_path_str, path);
     todo!()
 }
 
