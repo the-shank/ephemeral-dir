61fd839e9d00e9c38edb423221e956b313dc2c51
Shank <shashanksharma21@gmail.com>
2025-06-19 01:18:09 -0400

    add wrapper functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
 use std::fs;
 use std::path::{Path, PathBuf};
 
+pub fn ephemeral_dir(path: impl AsRef<Path>) -> eyre::Result<EphemeralDir> {
+    EphemeralDir::new(path)
+}
+
+pub fn ephemeral_dir_forced(path: impl AsRef<Path>) -> eyre::Result<EphemeralDir> {
+    EphemeralDir::new_forced(path)
+}
+
 pub struct EphemeralDir {
     // TODO: should we use a Box<Path> here instead of PathBuf?
     path: PathBuf,
@@ -53,6 +61,33 @@ fn test_dir_exists_after_creation() {
 fn test_dir_does_not_exist_after_going_out_of_scope() {
     let path = "/tmp/ephemeral_dir_test";
     let res_dir = EphemeralDir::new_forced(path);
+    assert!(res_dir.is_ok());
+    {
+        let Ok(dir) = res_dir else { unreachable!() };
+        assert!(dir.path().exists());
+        let dir_path_str = format!("{}", dir.path().display());
+        assert_eq!(dir_path_str, path);
+        let dir_path = Path::new(&dir_path_str);
+        assert!(dir_path.exists());
+    }
+    let path_path = Path::new(path);
+    assert!(!path_path.exists());
+}
+
+#[test]
+fn test_wrapper_functions() {
+    // ephemeral_dir::ephemeral_dir()
+    let path = "/tmp/ephemeral_dir_test";
+    let res_dir = ephemeral_dir(path);
+    assert!(res_dir.is_ok());
+    let Ok(dir) = res_dir else { unreachable!() };
+    assert!(dir.path().exists());
+    let dir_path_str = format!("{}", dir.path().display());
+    assert_eq!(dir_path_str, path);
+
+    // ephemeral_dir::ephemeral_dir_forced()
+    let path = "/tmp/ephemeral_dir_test";
+    let res_dir = ephemeral_dir_forced(path);
     assert!(res_dir.is_ok());
     {
         let Ok(dir) = res_dir else { unreachable!() };
